b9a67d57e65f2baea16cc7de93567ea545956732
polimatilda <polimatilda@gmail.com>
2023-01-14 19:51:24 +0100

    landing page styled

--- my-webpage/src/pages/LandingPage.jsx
+++ my-webpage/src/pages/LandingPage.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
+import './LandingPage.css'
+import profileColored from '../pictures/profileColored.jpg';
 
 function LandingPage() {
   return (
     <div>
       <Navbar/>
       LandingPage
+      <div className='landing-page-main-div'>
+        <div className='landing-pic'>
+          <img src={profileColored} alt="me on landing page" />
+        </div>
+        <div className='landing-textbox'>
+          <h1>Welcome to my page!</h1>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque quibusdam esse, reprehenderit vel error enim voluptatem suscipit ut porro sed corrupti provident perspiciatis est rem commodi omnis sapiente expedita culpa.</p>
+        </div>
+      </div>
       <Footer/>
     </div>
   )
