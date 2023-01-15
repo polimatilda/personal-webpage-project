3d8da0c91c9df98ce09b8333d06b77145ceace72
polimatilda <polimatilda@gmail.com>
2023-01-15 14:16:39 +0100

    button added to landind page

--- README.md
+++ README.md
@@ -5,3 +5,10 @@ https://colorhunt.co/palette/f8ede3dfd3c3d0b8a87d6e83
 
 https://colorhunt.co/palette/f5e8c7ecccb2deb6abac7088
 https://colorhunt.co/palette/f0ebe3e4dccf7d9d9c576f72
+
+examp page:
+https://colorlib.com/wp/template/elen/
+
+fonts:
+https://fonts.google.com/specimen/Quattrocento?query=Quattrocento
+https://fonts.google.com/specimen/Lora
--- my-webpage/src/pages/LandingPage.jsx
+++ my-webpage/src/pages/LandingPage.jsx
@@ -3,22 +3,31 @@ import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 import './LandingPage.css'
 import profileColored from '../pictures/profileColored.jpg';
+import { Link } from 'react-router-dom';
 
 function LandingPage() {
   return (
     <div>
-      <Navbar/>
+      <Navbar />
       LandingPage
       <div className='landing-page-main-div'>
         <div className='landing-pic'>
           <img src={profileColored} alt="me on landing page" />
         </div>
         <div className='landing-textbox'>
-          <h1>Welcome to my page!</h1>
+          <h1 className='landing-h1'>Welcome to my page!</h1>
           <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque quibusdam esse, reprehenderit vel error enim voluptatem suscipit ut porro sed corrupti provident perspiciatis est rem commodi omnis sapiente expedita culpa.</p>
+          <Link to="/about">
+          <button className='landing-btn'>
+            <h4>More about me</h4>
+            <span class="material-icons md-18">
+              arrow_forward_ios
+            </span>
+          </button>
+          </Link>
         </div>
       </div>
-      <Footer/>
+      <Footer />
     </div>
   )
 }
