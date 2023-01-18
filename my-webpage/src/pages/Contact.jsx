bdc0bf6444c4aa8d1ebd9a7ee637ecd8afd996d9
polimatilda <polimatilda@gmail.com>
2023-01-18 09:52:05 +0100

    contacts added, about positioning fixed

--- my-webpage/src/pages/Contact.jsx
+++ my-webpage/src/pages/Contact.jsx
@@ -1,13 +1,56 @@
 import React from 'react'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
+import './Contact.css'
+import { Fade } from "react-awesome-reveal"
 
 function Contact() {
   return (
     <div>
-      <Navbar/>
-      Contact
-      <Footer/>
+      <Navbar />
+      <div className='contact-main-div'>
+        <Fade triggerOnce={true} duration={2000} direction={"left"}>
+          <button className="contact-item">
+            <a href="mailto: example@gmail.com" target="_blank" rel='noreferrer'>
+              <span className="material-icons md-48">
+                mail
+              </span>
+              <h2>example@gmail.com</h2>
+            </a>
+          </button>
+        </Fade>
+        <Fade triggerOnce={true} duration={2000} direction={"left"}>
+          <button className="contact-item">
+            <a href="https://www.instagram.com/polimatilda/" target="_blank" rel='noreferrer'>
+              <span className="material-icons md-48">
+                photo_camera
+              </span>
+              <h2>Instagram</h2>
+            </a>
+          </button>
+        </Fade>
+        <Fade triggerOnce={true} duration={2000} direction={"left"}>
+          <button className="contact-item">
+            <a href="https://moly.hu/tagok/polimatilda" target="_blank" rel='noreferrer'>
+              <span className="material-icons md-48">
+                auto_stories
+              </span>
+              <h2>Moly.hu</h2>
+            </a>
+          </button>
+        </Fade>
+        <Fade triggerOnce={true} duration={2000} direction={"left"}>
+          <button className="contact-item">
+            <a href="https://linktr.ee/polimatilda" target="_blank" rel='noreferrer'>
+              <span className="material-icons md-48">
+                forest
+              </span>
+              <h2>Linktree</h2>
+            </a>
+          </button>
+        </Fade>
+      </div>
+      <Footer />
     </div>
   )
 }
--- my-webpage/src/pages/Social.jsx
+++ my-webpage/src/pages/Social.jsx
@@ -3,6 +3,7 @@ import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 import meWithBooks from '../pictures/meWithBooks.jpg'
 import polccast from '../pictures/polccast.jpg'
+import { Fade } from "react-awesome-reveal"
 import './Social.css';
 
 function Social() {
@@ -10,40 +11,44 @@ function Social() {
     <div>
       <Navbar />
       <div className='social-main-div'>
-        <div className='social-section youtube'>
-          <img src={meWithBooks} alt="youtube index" />
-          <div className='social-text'>
-            <h1 className='social-title'>YouTube channel</h1>
-            <p className='social-description'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore totam quibusdam iure sapiente voluptates quas cupiditate? Nesciunt aut quia corrupti aliquid possimus fuga, magnam quam iusto natus placeat dolorum sapiente!</p>
-            <div className='social-btn'>
-              <a href="https://www.youtube.com/matildaP" target="_blank" rel='noreferrer'>
-                <button className='social-link-btn'>
-                  <h4>Open channel</h4>
-                  <span className="material-icons md-18">
-                    arrow_forward_ios
-                  </span>
-                </button>
-              </a>
+        <Fade triggerOnce={true} duration={2000} direction={"left"}>
+          <div className='social-section youtube'>
+            <img src={meWithBooks} alt="youtube index" />
+            <div className='social-text'>
+              <h1 className='social-title'>YouTube channel</h1>
+              <p className='social-description'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore totam quibusdam iure sapiente voluptates quas cupiditate? Nesciunt aut quia corrupti aliquid possimus fuga, magnam quam iusto natus placeat dolorum sapiente!</p>
+              <div className='social-btn'>
+                <a href="https://www.youtube.com/matildaP" target="_blank" rel='noreferrer'>
+                  <button className='social-link-btn'>
+                    <h4>Open channel</h4>
+                    <span className="material-icons md-18">
+                      arrow_forward_ios
+                    </span>
+                  </button>
+                </a>
+              </div>
             </div>
           </div>
-        </div>
-        <div className='social-section polccast'>
-          <img src={polccast} alt="polccast index" />
-          <div className='social-text'>
-            <h1 className='social-title'>PolcCast podcast</h1>
-            <p className='social-description'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore totam quibusdam iure sapiente voluptates quas cupiditate? Nesciunt aut quia corrupti aliquid possimus fuga, magnam quam iusto natus placeat dolorum sapiente!</p>
-            <div className='social-btn'>
-              <a href="https://linktr.ee/polc.cast" target="_blank" rel='noreferrer'>
-                <button className='social-link-btn'>
-                  <h4>Open podcast</h4>
-                  <span className="material-icons md-18">
-                    arrow_forward_ios
-                  </span>
-                </button>
-              </a>
+        </Fade>
+        <Fade triggerOnce={true} duration={2000} direction={"left"}>
+          <div className='social-section polccast'>
+            <img src={polccast} alt="polccast index" />
+            <div className='social-text'>
+              <h1 className='social-title'>PolcCast podcast</h1>
+              <p className='social-description'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore totam quibusdam iure sapiente voluptates quas cupiditate? Nesciunt aut quia corrupti aliquid possimus fuga, magnam quam iusto natus placeat dolorum sapiente!</p>
+              <div className='social-btn'>
+                <a href="https://linktr.ee/polc.cast" target="_blank" rel='noreferrer'>
+                  <button className='social-link-btn'>
+                    <h4>Open podcast</h4>
+                    <span className="material-icons md-18">
+                      arrow_forward_ios
+                    </span>
+                  </button>
+                </a>
+              </div>
             </div>
           </div>
-        </div>
+        </Fade>
       </div>
       <Footer />
     </div>
